b56c3fad80870ec02bda95d055c2d1f6da45f13c
Kaito Akita <kaitokun07@icloud.com>
2023-08-28 00:25:23 +0900

    add keyball mouse layer

--- keyboards/keyball44/keymaps/develop/config.h
+++ keyboards/keyball44/keymaps/develop/config.h
@@ -31,3 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_EFFECT_ALTERNATING
 #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
+
+#define TAP_CODE_DELAY 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define SPLIT_USB_TIMEOUT 2500

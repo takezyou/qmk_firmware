2cbf377e56f193c89055612838d4530b1191d446
Kaito Akita <kaitokun07@icloud.com>
2023-09-18 22:36:55 +0900

    add

--- keyboards/keyball/lib/duplexmatrix/duplexmatrix.h
+++ keyboards/keyball/lib/duplexmatrix/duplexmatrix.h
@@ -1,6 +1,4 @@
 /*
-This is the c configuration file for the keymap
-
 Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
 
 This program is free software: you can redistribute it and/or modify
@@ -19,19 +17,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#endif
-
-#define TAP_CODE_DELAY 5
-#define DYNAMIC_KEYMAP_LAYER_COUNT 7
-#define SPLIT_USB_TIMEOUT 2500
+void duplex_scan_raw_post_kb(matrix_row_t out_matrix[]);
